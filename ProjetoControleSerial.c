1b977d293a2832d41570566f14cc917c8c48695e
Guilherme <202311190034@ifba.edu.br>
2025-01-17 14:41:13 -0300

    Projeto Base

---
+++ ProjetoControleSerial.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+#define gpio_buzzer 10
+#define gpio_led_green 11
+#define gpio_led_blue 12
+#define gpio_led_red 13
+
+void piscar_leds(int gpio_led){
+    // Liga o led
+}
+
+void tocar_buzzer(){
+    // Liga o buzzer
+}
+
+void desligar_leds(){
+    // Desliga os leds
+}
+
+void leitura_serial(){
+    // Lê a serial
+}
+
+void bootsel(){
+    // Verifica se o botão de seleção foi pressionado
+}
+
+int main()
+{
+    stdio_init_all();
+    gpio_init(gpio_buzzer);
+    gpio_init(gpio_led_green);
+    gpio_init(gpio_led_blue);
+    gpio_init(gpio_led_red);
+    gpio_set_dir(gpio_buzzer, GPIO_OUT);
+    gpio_set_dir(gpio_led_green, GPIO_OUT);
+    gpio_set_dir(gpio_led_blue, GPIO_OUT);
+    gpio_set_dir(gpio_led_red, GPIO_OUT);
+
+    while (true) {
+        //Aguardando Código
+        sleep_ms(1000);
+    }
+}
