b6693d39b85b72d0ed96e41eead5e0d8eb4544fe
Leonam Rabelo <rabeloleonam@gmail.com>
2025-01-18 19:27:13 -0300

    Correção da tecla para ativar o buzzer

--- ProjetoControleSerial.c
+++ ProjetoControleSerial.c
@@ -59,10 +59,10 @@ int main()
 
     while(1){
         //Recebe comandos via UART
-        printf("Digite um comando (G: Green, B: Blue, R: Red, W: White, T: Buzzer, O: Off): ");
+        printf("Digite um comando (G: Green, B: Blue, R: Red, W: White, Z: Buzzer, O: Off): ");
         tecla = getchar(); //Aguarda comando do terminal
 
-        if(tecla=='B'){          //Caso a tecla enviada for B  
+        if(tecla=='Z'){          //Caso a tecla enviada for Z
             tocar_buzzer();     //Chamar funcao para ativar o Buzzer
         }else if(tecla=='T'){
             bootsel();
