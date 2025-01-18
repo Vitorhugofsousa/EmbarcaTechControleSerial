511605552b037146ae8cfea55cf3b22f8d831911
Leonam Rabelo <rabeloleonam@gmail.com>
2025-01-18 02:01:33 -0300

    Criando funcao base para inicializacao dos pinos

--- ProjetoControleSerial.c
+++ ProjetoControleSerial.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-#define gpio_buzzer 10
+#define gpio_buzzer 21
 #define gpio_led_green 11
 #define gpio_led_blue 12
 #define gpio_led_red 13
+
+void inicializa_pinos(){
+    gpio_init(gpio_led_green);
+    gpio_set_dir(gpio_led_green, GPIO_OUT);
+
+    gpio_init(gpio_led_blue);
+    gpio_set_dir(gpio_led_blue, GPIO_OUT);
+
+    gpio_init(gpio_led_red);
+    gpio_set_dir(gpio_led_red, GPIO_OUT);
+
+    gpio_init(gpio_buzzer);
+    gpio_set_dir(gpio_buzzer, GPIO_OUT);
+}
 
 void piscar_leds(int gpio_led){
     // Liga o led
@@ -29,16 +43,9 @@ void bootsel(){
 int main()
 {
     stdio_init_all();
-    gpio_init(gpio_buzzer);
-    gpio_init(gpio_led_green);
-    gpio_init(gpio_led_blue);
-    gpio_init(gpio_led_red);
-    gpio_set_dir(gpio_buzzer, GPIO_OUT);
-    gpio_set_dir(gpio_led_green, GPIO_OUT);
-    gpio_set_dir(gpio_led_blue, GPIO_OUT);
-    gpio_set_dir(gpio_led_red, GPIO_OUT);
+    inicializa_pinos();
 
-    while (true) {
+    while(1){
         //Aguardando Código
         sleep_ms(1000);
     }
